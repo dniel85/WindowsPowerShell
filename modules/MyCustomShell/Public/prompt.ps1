e332cad43035d1bc36d504adae73b507eb7c5653
Nielsen <darrell.nielsen@MEL.DOM>
2026-03-05 11:59:46 -0600

    added repo feature to prompt value of custon shell and cleaned up scripts

--- modules/MyCustomShell/Public/prompt.ps1
+++ modules/MyCustomShell/Public/prompt.ps1
@@ -1,4 +1,23 @@
 ﻿function prompt {
+    #detect repo/branch
+    $git = $null
+    $path = $PWD.Path
+    while ($path) {
+        $head = Join-Path $path ".git\HEAD"
+        if (Test-Path $head) {
+            $repo = Split-Path $path -Leaf
+            $line = [System.IO.File]::ReadLines($head) | Select-Object -First 1
+            if ($line -match "refs/heads/(.+)") {
+                $git = "$repo/$($matches[1])"
+            }
+            else {
+                $git = $repo
+            }
+            break
+        }
+        $path = Split-Path $path
+    }
+    #path formatting
     $currentPath = (Get-Location).Path
     $shome = [Environment]::GetFolderPath('UserProfile')
     if ($currentPath -like "$shome*") {
@@ -7,16 +26,21 @@
     else {
         $currentPath = $currentPath -replace '\\','/'
     }
+    #admin detection
     $isAdmin = ([Security.Principal.WindowsPrincipal] `
         [Security.Principal.WindowsIdentity]::GetCurrent()
     ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    #prompt
     Write-Host "[" -ForegroundColor DarkYellow -NoNewline
+    if ($git) {
+        Write-Host "repo:$git " -ForegroundColor Magenta -NoNewline
+    }
     Write-Host "$env:USERNAME@$env:COMPUTERNAME " -ForegroundColor Cyan -NoNewline
     if ($isAdmin) {
         Write-Host "ADMIN" -ForegroundColor Red -NoNewline
     }
-    if($PSVersionTable.PSVersion.Major -eq 7){
-        write-host " PS7" -ForegroundColor DarkCyan -nonewline
+    if ($PSVersionTable.PSVersion.Major -eq 7) {
+        Write-Host " PS7" -ForegroundColor DarkCyan -NoNewline
     }
     Write-Host " $currentPath]" -ForegroundColor DarkYellow -NoNewline
     Write-Host "$ " -ForegroundColor DarkYellow -NoNewline
